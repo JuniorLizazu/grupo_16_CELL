2d25ffdbb0dc27b471eac01b27cb319d449d61a2
JuniorLizazu <cacereslizazu2@hotmail.com>
2020-09-30 17:37:53 -0300

    finalizacion de formulario registro

--- site/controllers/usersControllers.js
+++ site/controllers/usersControllers.js
@@ -1,9 +1,12 @@
 const path = require('path');
+const fs = require('fs');
+const bcrypt = require('bcrypt');
+
+const {validationResult} = require('express-validator');
+
+
 const dbProducts = require(path.join(__dirname,'..','data','dbProducts'))
 const dbUsers = require(path.join(__dirname,'..','data','dbUsers'))
-const fs = require('fs');
-
-const bcrypt = require('bcrypt');
 
 module.exports={
     register:function(req,res){
--- site/routes/users.js
+++ site/routes/users.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-const usersController = require('../controllers/usersControllers');
+
+const usersController = require('../controllers/usersController')
 
 const upImagesUsers = require('../middlewares/upImagesUsers')
+const registerValidator = require('../validations/registerValidator')
+const loginValidator = require('../validations/loginValidator');
 
 /* GET users listing. */
 
 
 router.get('/register', usersController.register);
+router.post('/register',upImagesUsers.any(),registerValidator,usersController.processRegister);
 
 router.get('/login', usersController.login);
 router.post('/login', usersController.processLogin);
