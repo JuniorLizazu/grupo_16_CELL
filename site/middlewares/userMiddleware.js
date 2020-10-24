dbe1810418104799e80c421adf8ddb49bd2082ce
Nelson Barrios <Nelson.barrios3@gmail.com>
2020-10-23 23:28:15 -0300

    Update userMiddleware.js
    Renderización de usuario

--- site/middlewares/userMiddleware.js
+++ site/middlewares/userMiddleware.js
@@ -1,8 +1,8 @@
 module.exports = (req, res, next)=>{
     if(req.session.user != undefined){
-        res.render('profile.ejs', {
-            title: "Perfil",
-            css: 'profile.css'
+        res.render('index.ejs', {
+            title: "Bienvenidos a Cell",
+            css: 'home.css'
         })
     } else {
         next();
