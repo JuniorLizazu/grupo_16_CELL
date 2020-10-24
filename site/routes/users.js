79b0957019eea3ce546ab3bd9ec2b97c2b00e274
Nelson Barrios <Nelson.barrios3@gmail.com>
2020-10-23 23:33:25 -0300

    Update users.js
    Implementación de userMiddleware a register y a login.

--- site/routes/users.js
+++ site/routes/users.js
@@ -7,16 +7,17 @@ const usersController = require('../controllers/usersControllers')
 const registerValidator = require('../validations/registerValidator')
 const loginValidator = require('../validations/loginValidator');
 
-const upImagesUsers = require('../middlewares/upImagesUsers')
+const upImagesUsers = require('../middlewares/upImagesUsers');
 const huespedMiddleware = require('../middlewares/huespedMiddleware');
+const userMiddleware = require('..middlewares/userMiddleware');
 
 /* GET users listing. */
 
 
-router.get('/register', usersController.register);
+router.get('/register',userMiddleware, usersController.register);
 router.post('/register',upImagesUsers.any(),registerValidator,usersController.processRegister);
 
-router.get('/login', usersController.login);
+router.get('/login',userMiddleware, usersController.login);
 router.post('/login', loginValidator,usersController.processLogin);
 
 router.get('/profile',huespedMiddleware, usersController.profile);
