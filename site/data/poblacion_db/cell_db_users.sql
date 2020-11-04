192c8a25bfa614772566b43a68e7572521f3aa35
Nelson Barrios <Nelson.barrios3@gmail.com>
2020-11-04 11:41:26 -0300

    Update cell_db_users.sql
    Poblacion

--- site/data/poblacion_db/cell_db_users.sql
+++ site/data/poblacion_db/cell_db_users.sql
@@ -45,13 +45,6 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Santiago','Ojeda','santiagoeojeda@gmail.com','avatar-avatar-1601660125002.jpg','$2b$10$Nl9wkjyUZEl6TGgY/Z9TXeHHDq9.HVduEnari31YPFnHNRnoPaCKi','admin',NULL,NULL,NULL,NULL,NULL,NULL),(2,'Juan Junior','Caceres Lizazu','cacereslizazu2@hotmail.com','avatar-avatar-1601840038330.jfif','$2b$10$jEdQy.6Vry.fk/VWUawU4eVCrBkqdhwCKoyD3GI8RXEDqjixtsccW','admin',NULL,NULL,NULL,NULL,NULL,NULL),(3,'Nelson','Barrios','nelson.barrios3@gmail.com','avatar-avatar-1603512441962.jpeg','$2b$10$X5wRXOETNSaEf6e8.JE3aOeqR8k.7rMFIKb0ECASeJW5/nAA/4E36','admin',NULL,NULL,NULL,NULL,NULL,NULL),(4,'Roberto','Ayala','roberto@ayala.com','default.png','$2b$10$aLXjP1FkBguSIg.983q1heNkfgA3NHiqPK0MbBaNX6NwrZEhF.rgS','user',NULL,NULL,NULL,NULL,NULL,NULL);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
